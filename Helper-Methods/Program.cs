4782ae789669d3a656035b4ba99846502eeb7cf3
Therwig6 <31357508+Therwig6@users.noreply.github.com>
2017-09-17 20:20:08 -0600

    Initial commit

---
+++ Helper-Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelperMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string myValue = superSecretFormula();
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+        //Private allows for the method to be used only in this spacefic class, just a code block with a name is a method
+        private static string superSecretFormula()
+        {
+            //some cool stuff here
+            //
+            return "hello world";
+        }
+
+
+
+    }
+}
